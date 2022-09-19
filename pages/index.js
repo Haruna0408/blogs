f37ed2ef223f550edfaa8b008d5fa3db0e9799ce
Haruna0408 <n2.m5.1f@gmail.com>
2022-09-20 02:12:56 +0900

    作って学ぶNext.js/React Webサイト構築 Chapter 4.5まで制作

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+// import React from "react";
+import Container from "/components/container";
+import Hero from "/components/hero";
+
+export default function Home() {
+  return <Hero title="CUBE" subtitle="アウトプットしていくサイト" imageOn />;
+}
