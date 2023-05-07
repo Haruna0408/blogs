6a172acfb229ae722a435130068c5356359a61e4
Haruna0408 <n2.m5.1f@gmail.com>
2023-05-07 18:49:22 +0900

    Chapter6 メタデータ まで終了

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
 // import React from "react";
+import Meta from "components/meta";
 import Container from "/components/container";
 import Hero from "/components/hero";
 
 export default function Home() {
-  return <Hero title="CUBE" subtitle="アウトプットしていくサイト" imageOn />;
+  return (
+    <>
+      <Container>
+        <Meta />
+        <Hero title="CUBE" subtitle="アウトプットしていくサイト" imageOn />
+      </Container>
+    </>
+  );
 }
