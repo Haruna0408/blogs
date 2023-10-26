d908968eb59906f55ce6e7e466eea1865e62b5d7
Haruna0408 <n2.m5.1f@gmail.com>
2023-10-26 22:33:49 +0900

    next/imageをnext/legacy/image/へ変更

--- pages/about.js
+++ pages/about.js
@@ -9,7 +9,7 @@ import {
   TwoColumnMain,
   TwoColumnSidebar,
 } from "/components/two-column";
-import Image from "next/image";
+import Image from "next/legacy/image";
 import eyecatch from "images/about.jpg";
 
 export default function About() {
