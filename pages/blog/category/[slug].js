db0cb95738d39ad81b556c4e0251c6bf36c8c93d
Haruna0408 <n2.m5.1f@gmail.com>
2023-10-28 22:11:03 +0900

    chapt9-10: カテゴリーページに記事一覧を表示する

--- pages/blog/category/[slug].js
+++ pages/blog/category/[slug].js
@@ -1,11 +1,17 @@
-import { getAllCategories } from "lib/api";
+import { getAllCategories, getAllPostsByCategory } from "lib/api";
 import Container from "components/container";
 import PostHeader from "components/post-header";
+import Posts from "components/posts";
+//import { GetPlaiceholder } from "plaiceholder";
 
-export default function Category({ name }) {
+import { eyecatchLocal } from "lib/constants";
+
+export default function Category({ name, posts }) {
   return (
     <Container>
       <PostHeader title={name} subtitle="Blog Category" />
+
+      <Posts posts={posts} />
     </Container>
   );
 }
@@ -25,9 +31,20 @@ export async function getStaticProps(context) {
   const allCats = await getAllCategories();
   const cat = allCats.find(({ slug }) => slug === catSlug);
 
+  const posts = await getAllPostsByCategory(cat.id);
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty("eyecatch")) {
+      post.eyecatch = eyecatchLocal;
+    }
+    // const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    // post.eyecatch.blurDataURL = base64
+  }
+
   return {
     props: {
       name: cat.name,
+      posts: posts,
     },
   };
 }
